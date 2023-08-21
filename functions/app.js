9725c840a346c70554298b977a800d3224917adb
Jose Luis Astorga Morillo <joseluastorga97@gmail.com>
2023-08-22 00:13:23 +0200

    cambios de netlify

--- functions/app.js
+++ functions/app.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(express.json())
 app.use('/productos', productosRouter);
 app.use('/prices', historicoPreciosRouter);
-app.use('/.netlify/functions/app', router)
+//app.use('/.netlify/functions/app', router)
 module.exports.handler = serverless(app)
 
 //Rutas
